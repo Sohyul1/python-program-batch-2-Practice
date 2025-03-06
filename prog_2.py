eb534d33948b67f4d1f0f61c783c002d1e6bd11f
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:21:14 +0800

    algo

---
+++ prog_2.py
@@ -0,0 +1,4 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# Ask for ussr input 
+# Determine if the numbers are equal or not
+# PRint the output
