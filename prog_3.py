c9a2c53bccfc3b0a90fb6ede00cb476e18e28d70
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:32:28 +0800

    done

--- prog_3.py
+++ prog_3.py
@@ -1,4 +1,15 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 # Ask for user inpit
+while True:
+    num1 = float(input("Enter a number: "))
+    num2 = float(input("One more:: "))
+
 # Determine the difference
-# PRint the output
+    diff = num1 - num2
+    #print the output
+    print(f"The diffrence is:  {diff}")
+
+    ans = input("Repeat?(y/n): ").lower()
+    if ans == "n":
+        print("Good Bye!")
+
