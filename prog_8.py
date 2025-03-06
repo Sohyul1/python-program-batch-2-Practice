3fb2e45f9399592d59e57ec431a2d6df3a7af64b
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 14:41:31 +0800

    done

--- prog_8.py
+++ prog_8.py
@@ -1,5 +1,14 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 # Make an empty list
+empt_list = []
+num = 0
+
 # Make the while loop
-# Filter out the odd nmbers
+while num <= 100:
+   # Filter out the odd nmber
+    if num % 2 != 0:
+        empt_list.append(num)
+    num += 1
+
 # Print all odd numbers
+print(f"The odd numbers from 0 to 100 are:\n\n{empt_list}")
