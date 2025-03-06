102ff7023ea6896c8c395b5305ce95c1b685163a
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:50:46 +0800

    done

--- prog_5.py
+++ prog_5.py
@@ -1,4 +1,12 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 # ASk for user input
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second: "))
 # Do a modulus division
+    result = num1 % num2
 # print the results
+    print(f"The remainder is {result}")
+    ans = input("Repeat?(y/n): ").lower()
+    if ans == "n":
+        print("Good Bye!")
