e0c1dd0a63048c76ec1f5893238d94345da00559
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:46:00 +0800

    done

--- prog_4.py
+++ prog_4.py
@@ -1,4 +1,13 @@
 # Prog4: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 # Ask fr user input
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second: "))
 # GEt the quotient
+    quotient = int(num1 // num2)
 # Print the output(without decimal)
+    print(f"The quotient is:  {quotient}")
+    
+    ans = input("Repeat?(y/n): ").lower()
+    if ans == "n":
+        print("Good Bye!")
