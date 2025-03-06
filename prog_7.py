61430f17720433c8371087629ebf5471ebcaad7c
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 14:11:40 +0800

    algov2

--- prog_7.py
+++ prog_7.py
@@ -1,4 +1,5 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
-# Ask for user input 
+# MAke an empty list
+# Ask for user input
 # Filter out the even numbers
 # Print the output
