7ab8a4fdedace744f8a015a3336bb1c2242a4279
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 14:34:56 +0800

    algo

---
+++ prog_8.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+# Make an empty list
+# Make the while loop
+# Filter out the odd nmbers
+# Print all odd numbers
