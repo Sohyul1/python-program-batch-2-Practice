2bef81ee6c29109bc1842d4840856a1c47fe2419
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:25:48 +0800

    done

--- prog_2.py
+++ prog_2.py
@@ -1,4 +1,16 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 # Ask for ussr input 
+while True:
+    num1 = float(input("Enter a number: "))
+    num2 = float(input("One more:: "))
+
 # Determine if the numbers are equal or not
+    if num1 != num2:
+        print("Not Equal")
+    else:
+        print(" ")
+
+    ans = input("Repeat?(y/n): ").lower()
+    if ans == "n":
+        print("Good Bye!")
 # PRint the output
