68f486f7f8f12e42bf47b09f444d6a19e255c141
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 14:47:36 +0800

    algo

---
+++ prog_9.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+# Make the for loop
+# Filter out the numbers ending with 0 or 5
+# Print the rest of the numbers 
