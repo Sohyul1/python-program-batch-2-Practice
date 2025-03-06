7db944dece9058177d8d343ea0ac69678bf0b5ec
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 15:14:06 +0800

    done

--- prog_10.py
+++ prog_10.py
@@ -1,4 +1,14 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 # Ask for user input
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second: "))
 # Get the numbers between
-# Print the output
+    start = int(min(num1, num2))
+    end = int(max(num1, num2))
+    for i in range (start + 1, end ):
+       # Print the output
+        print(i)
+    ans = input("Repeat?(y/n): ").lower()
+    if ans == "n":
+        print("Good Bye!")
