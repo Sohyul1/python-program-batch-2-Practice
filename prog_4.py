521c92e2164d0854dc996f0f682748a7f17f73f3
Sohyul1 <aerondequito967@gmail.com>
2025-03-06 13:34:12 +0800

    algo

---
+++ prog_4.py
@@ -0,0 +1,4 @@
+# Prog4: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+# Ask fr user input
+# GEt the quotient
+# Print the output(without decimal)
